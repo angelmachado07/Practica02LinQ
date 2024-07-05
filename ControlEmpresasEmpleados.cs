d0b90b7b114eea32f4b9dd8dbd1500ef84780073
angelmachado07 <angelmachadosouza@gmail.com>
2024-07-05 11:54:52 -0300

    Agregar archivos de proyecto.

---
+++ ControlEmpresasEmpleados.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica02LinQ
+{
+    internal class ControlEmpresasEmpleados
+    {
+        public List<Empresa> listaEmpresas; 
+        public List<Empleado> listaEmpleados;
+
+        public ControlEmpresasEmpleados()
+        {
+            listaEmpresas = new List<Empresa>();
+            listaEmpleados = new List<Empleado>();
+
+            listaEmpresas.Add(new Empresa { Id = 1, Nombre = "IAlpha" });
+            listaEmpresas.Add(new Empresa { Id = 2, Nombre = "Udelar" });
+            listaEmpresas.Add(new Empresa { Id = 3, Nombre = "SpaceZ" });
+
+            listaEmpleados.Add(new Empleado { Id = 1, Nombre = "Gonzalo", Cargo = "CEO", EmpresaId = 1, Salario = 4000});
+            listaEmpleados.Add(new Empleado { Id = 2, Nombre = "JuanC", Cargo = "Desarrollador", EmpresaId = 1, Salario = 2500 });
+            listaEmpleados.Add(new Empleado { Id = 3, Nombre = "JuanR", Cargo = "Desarrollador", EmpresaId = 1, Salario = 2000 });
+            listaEmpleados.Add(new Empleado { Id = 4, Nombre = "Daniel", Cargo = "Desarrollador", EmpresaId = 1, Salario = 2000 });
+            listaEmpleados.Add(new Empleado { Id = 5, Nombre = "GonzaloT", Cargo = "CEO", EmpresaId = 2, Salario = 2000 });
+            listaEmpleados.Add(new Empleado { Id = 6, Nombre = "Leonardo", Cargo = "CEO", EmpresaId = 1, Salario = 3000 });
+            listaEmpleados.Add(new Empleado { Id = 1, Nombre = "Gonzalo", Cargo = "CEO", EmpresaId = 3, Salario = 3000 });
+            listaEmpleados.Add(new Empleado { Id = 6, Nombre = "Leonardo", Cargo = "CEO", EmpresaId = 3, Salario = 3000 });
+        }
+
+        public void getCEO(string Cargo)
+        {
+            IEnumerable<Empleado> empleados = from empleado in listaEmpleados
+                                              where empleado.Cargo == Cargo
+                                              select empleado;
+            foreach (Empleado elemento in empleados)
+            {
+                elemento.getDatosEmpleado();
+            }
+        }
+
+        public void getEmpleadosOrdenados()
+        {
+            IEnumerable<Empleado> empleados = from empleado in listaEmpleados
+                                              orderby empleado.Nombre
+                                              select empleado;
+            foreach (Empleado elemento in empleados)
+            {
+                elemento.getDatosEmpleado();
+            }
+        }
+
+        public void getEmpleadoOrdenadosSegun()
+        {
+            IEnumerable<Empleado> empleados = from empleado in listaEmpleados
+                                              orderby empleado.Salario
+                                              select empleado;
+            foreach (Empleado elemento in empleados)
+            {
+                elemento.getDatosEmpleado();
+            }
+        }
+
+        public void getEmpleadosEmpresa(int _Empresa)
+        {
+            IEnumerable<Empleado> empleados = from empleado in listaEmpleados
+                                              join empresa in listaEmpresas on empleado.EmpresaId
+                                              equals empresa.Id
+                                              where empresa.Id == _Empresa
+                                              select empleado;
+            foreach (var item in empleados)
+            {
+                item.getDatosEmpleado();
+            }
+        }
+
+        //_________ Metodos Particulares __________
+
+        public void promedioSalario()
+        {
+            var consulta = from e in listaEmpleados
+                           group e by e.EmpresaId into g
+                           select new { empresa = g.Key, PromedioSalario = g.Average(e => e.Salario) };
+            foreach (var item in consulta)
+            {
+                switch (item.empresa)
+                {
+                    case 1:
+                        Console.WriteLine($"Empresa IAlpha - Promedio de salario : {item.PromedioSalario}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Empresa Udelar - Promedio de salario : {item.PromedioSalario}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"Empresa SpaceZ - Promedio de salario : {item.PromedioSalario}");
+                        break;
+
+                }
+            }
+        }
+
+        //EjerciciosFoto
+        public void getCantidadCEO(int empresaId)
+        {
+            IEnumerable<Empleado> ceos = from empleado in listaEmpleados
+                                         where empleado.Cargo == "CEO" && empleado.EmpresaId == empresaId
+                                         select empleado;
+
+            int cantidadCEOs = ceos.Count();
+
+            Console.WriteLine($"Cantidad de CEOs en la empresa con Id {empresaId}: {cantidadCEOs}");
+        }
+
+
+        public void getEmpleadoMasGana()
+        {
+            IEnumerable<Empleado> empleados = from empleado in listaEmpleados
+                                              orderby empleado.Salario descending
+                                              select empleado;
+
+            foreach (Empleado empleadoMasGana in empleados)
+            {
+                Console.WriteLine("\nEmpleado que gana más:");
+                empleadoMasGana.getDatosEmpleado();
+
+                break;
+            }
+        }
+
+        public void getEmpleadoGanaMas2200()
+        {
+            IEnumerable<Empleado> empleados = from empleado in listaEmpleados
+                                              where empleado.Salario > 2200
+                                              orderby empleado.Salario descending
+                                              select empleado;
+
+            Console.WriteLine("\nEmpleados que ganan más de 2200:\n");
+            foreach (Empleado empleadoMasGana in empleados)
+            {
+                empleadoMasGana.getDatosEmpleado();
+            }
+        }
+
+        public void getEmpleadoMasGanaPorCargo()
+        {
+            string[] cargos = { "CEO", "Desarrollador" };
+
+            foreach (string cargo in cargos)
+            {
+                IEnumerable<Empleado> empleados = from empleado in listaEmpleados
+                                                  where empleado.Cargo == cargo
+                                                  orderby empleado.Salario descending
+                                                  select empleado;
+
+                Console.WriteLine($"\n{cargo} que gana más:");
+                empleados.First().getDatosEmpleado();
+            }
+        }
+
+        public void getEmpleadoMasGanaPorEmpresa()
+        {
+            var empleadosPorEmpresa = from empleado in listaEmpleados
+                                      group empleado by empleado.EmpresaId into grupo
+                                      select grupo.OrderByDescending(e => e.Salario).FirstOrDefault();
+
+            Console.WriteLine("\nEmpleado que gana más por empresa:");
+            foreach (Empleado empleado in empleadosPorEmpresa)
+            {
+                empleado.getDatosEmpleado();
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica02LinQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcion;
+
+            do
+            {
+                Console.WriteLine("Elija una opcion");
+                Console.WriteLine("1 -- Ejercicio 1-a");
+                Console.WriteLine("2 -- Ejercicio 1-b");
+                Console.WriteLine("3 -- Ejercicio 2");
+                Console.WriteLine("0 -- Salir");
+                opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+
+                    case "1":
+                        //////Ejemplo
+                        //Console.WriteLine("ejemplo");
+                        //var consulta = from p in personas where p.Edad < 25 && p.Ciudad == "Lima" orderby p.Nombre descending select new { p.Nombre, p.Edad };
+                        //foreach (var persona in consulta)
+                        //{
+                        //    Console.WriteLine($"{persona.Nombre} ({persona.Edad} anos)");
+                        //}
+
+                        List<Persona> personas = new List<Persona>
+            {
+                new Persona { Nombre = "Juan", Edad = 25, Ciudad = "Lima" },
+                new Persona { Nombre = "Maria", Edad = 30, Ciudad = "Bogota" },
+                new Persona { Nombre = "Pedro", Edad = 35, Ciudad = "Madrid" },
+                new Persona { Nombre = "Ana", Edad = 20, Ciudad = "Lima" },
+                new Persona { Nombre = "Jose", Edad = 40, Ciudad = "Buenos Aires" }
+            };
+                        Console.WriteLine("Personas que tengan una edad mayor a 30 y vivan en Bogotá.");
+                        personas.Where(p => p.Edad >= 30 && p.Ciudad == "Bogota").ToList().ForEach(p => Console.WriteLine(p.Nombre));
+
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        break;
+                    case "0":
+                        break;
+                    default:
+                        Console.WriteLine("Error");
+                        break;
+                }
+                Console.ReadLine();
+                Console.Clear();
+
+            } while (opcion != "0");
+
+            //////Ejemplo
+            //Console.WriteLine("ejemplo");
+            //var consulta = from p in personas where p.Edad < 25 && p.Ciudad == "Lima" orderby p.Nombre descending select new { p.Nombre, p.Edad };
+            //foreach (var persona in consulta)
+            //{
+            //    Console.WriteLine($"{persona.Nombre} ({persona.Edad} anos)");
+            //}
+
+            //////Ejercicio 1 - a)
+            //Console.WriteLine("Personas que tengan una edad mayor a 30 y vivan en Bogotá.");
+            //personas.Where(p => p.Edad >= 30 && p.Ciudad == "Bogota").ToList().ForEach(p => Console.WriteLine(p.Nombre));
+
+            //////Ejercicio 1 - b)
+            //Console.WriteLine("Orden por edad (25 a 35)");
+            //personas.OrderBy(p => p.Edad).Where(p => p.Edad >= 25 && p.Edad <= 35).ToList().ForEach(p => Console.WriteLine(p.Nombre));
+            //---------------------------------
+
+            //Ejercicio 2
+
+            ControlEmpresasEmpleados ce = new ControlEmpresasEmpleados();
+
+            //Console.WriteLine("Promedio por empresas \n*");
+            //ce.promedioSalario();
+            //Console.WriteLine("");
+
+            //Console.WriteLine("Peces Gordos \n*");
+            //ce.getCEO("CEO");
+
+            //Console.WriteLine("");
+            //Console.WriteLine("Planillas \n*");
+            //ce.getEmpleadosOrdenados();
+            //Console.WriteLine("");
+            //Console.WriteLine("Planillas ordenadas por salario \n*");
+            //ce.getEmpleadoOrdenadosSegun();
+
+            //Console.WriteLine("\nIngrese la empresa: (entero 1 a 3)\n1 para IAlpha\n2 para Udelar\n3 para SpaceZ");
+            //string _Id = Console.ReadLine();
+            //try
+            //{
+            //    int _Empresa = int.Parse(_Id);
+            //    ce.getEmpleadosEmpresa(_Empresa);
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("Ha introducido un Id erroneo. Debe de ingresar  un numero entero");
+            //}
+
+            // Ejercicio 2.3 
+
+            //Console.WriteLine("Ingrese el ID de la empresa para ver la cantidad de CEOs:");
+            //if (int.TryParse(Console.ReadLine(), out int empresaId))
+            //{
+            //    var empresa = ce.listaEmpresas.FirstOrDefault(e => e.Id == empresaId);
+            //    if (empresa != null)
+            //    {
+            //        string nombreEmpresa = empresa.Nombre;
+            //        ce.getCantidadCEO(empresaId);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Error: No se encontró ninguna empresa con el ID ingresado.");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Error: Debe ingresar un número entero.");
+            //}
+
+            //Ejercicio 2.4
+
+            //ce.getEmpleadoMasGana();
+
+            //Ejercicio 2.5
+
+            //ce.getEmpleadoGanaMas2200();
+
+            //Ejercicio 2.6
+
+            //ce.getEmpleadoMasGanaPorCargo();
+
+            //Ejercicio 2.7
+
+            //ce.getEmpleadoMasGanaPorEmpresa();
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
